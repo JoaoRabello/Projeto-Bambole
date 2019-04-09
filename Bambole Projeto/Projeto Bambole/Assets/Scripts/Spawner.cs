ccc02f267ec97885487ff73887a3cb960c95aa78
Joao Rabello <rabellois@hotmail.com>
2019-04-09 16:50:16 -0300

    Leitura de Charts
    -O leitor de charts já spawna as notas de forma correta, mas vai ser necessário sincronizar os tempos e ler mais informações.
    -Ainda necessário ler os arquivos

--- Bambole Projeto/Projeto Bambole/Assets/Scripts/Spawner.cs
+++ Bambole Projeto/Projeto Bambole/Assets/Scripts/Spawner.cs
@@ -7,13 +7,14 @@ public class Spawner : MonoBehaviour {
     public float tempoS = 0;
     public float tempoPerm = 0;
     public int pos = 0;
+    public int linha = 0;
 
-    float timeToSpawn = 0f;
     float timer = 0f;
 
-    public string musicChart = "3000 = N 1 0\n7200 = S 3 0";
-    string[] linhas = new string[5];
-    string[,] nota = new string[10,10];
+    string musicChart = "3072 = N 2 0.3264 = N 2 0.3264 = N 3 0.4800 = N 4 0.4800 = N 0 0.4800 = N 3 0.6336 = N 1 0.6336 = N 2 06624 = N 1 0.6624 = N 2 0.6912 = N 1 0.6912 = N 2 0.7200 = N 1 0.7200 = N 2 0.7680 = N 1 0.7680 = N 3 0.7968 = N 1 0.7968 = N 3 0.8256 = N 1 0";
+    string[] linhas = new string[100];
+    string[,] nota = new string[100,100];
+    [SerializeField] GameObject[] notas = new GameObject[5];
 
 	void Start () {
 
@@ -22,7 +23,7 @@ public class Spawner : MonoBehaviour {
         linhas = musicChart.Split('.');
         for (int i = 0; i < linhas.Length; i++)
         {
-            string[] temp = new string[5];
+            string[] temp = new string[6];
             temp = linhas[i].Split(' ');
             for(int j = 0; j < 6; j++)
             {
@@ -30,12 +31,23 @@ public class Spawner : MonoBehaviour {
                 Debug.Log("Nota em " + i + " x " + j + ": " + nota[i, j]);
             }
         }
+
+        tempoS = float.Parse(nota[linha, 0]);
     }
 
     void Update () {
 
-        timer += (Time.deltaTime * 1000) % 1000;
-        Debug.Log(timer);
+        timer += Time.deltaTime * 1000;
+        if(tempoS <= timer && linha < linhas.Length)
+        {
+            Instantiate(notas[int.Parse(nota[linha, 4])]);
+            linha++;
+            Debug.Log(linha);
+            if(linha < linhas.Length)
+            {
+                tempoS = float.Parse(nota[linha, 0]);
+            }
+        }
 
 	}
 }
